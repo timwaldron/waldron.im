462e7b30c80d78409486278fc110fccca08f3f85
Timothy Waldron <tim@waldron.im>
2019-06-27 09:51:06 +1000

    Restarting using React, instead of Rails

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p><strong>waldron.im</strong> is being redesigned using React!</p>
+        <small>Thank you for your patience</small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
